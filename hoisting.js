485b33970e4bdc41744ccf91fb95cc1fd95bce26
nancy sin <nsin@sbcglobal.net>
2018-01-01 00:49:50 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,9 @@
-
-
-  function sayMy() {
-    var name = "Kristin";
-    console.log(name);
-  }
+function callMe() {
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+  var lyric = "maybe";
+  return lyric;
+}
